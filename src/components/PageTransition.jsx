1fe333173ba68245cd52b60ed7fe3b5c04bae36c
RyanS2025 <ryansinha@gmail.com>
2026-03-19 11:08:39 -0400

    Fixed transition to scroll to top

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 
 export default function PageTransition( { children }) {
+    useEffect(() => {window.scrollTo(0, 0);}, []);
+
     return (
         <motion.div
         initial={{ opacity: 0, y: 20 }}
